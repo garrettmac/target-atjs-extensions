7e3356f55a587a4d45beef74c9c1bc011eb3f0a4
garrettmac <garrettmacmac@gmail.com>
2017-11-02 14:38:37 -0700

    Update component.jsx

--- react/component/src/component.jsx
+++ react/component/src/component.jsx
@@ -33,9 +33,7 @@ export default function createTargetComponent(React, opts) {
       return onComponentMounted(this, this.at, this.logger);
     }
 
-    shouldComponentUpdate() {
-      return false;
-    }
+//    shouldComponentUpdate() { return false;}
 
     componentWillReceiveProps(newProps) {
       return onComponentWillReceiveProps(this, this.at, this.logger, newProps);
